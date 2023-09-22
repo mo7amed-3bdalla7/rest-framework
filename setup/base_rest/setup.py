d282d9f5ef653edeadd3ae89bb3ffdff60263ec8
Mohamed Abdallah <mo7amed-3bdalla7@users.noreply.github.com>
2023-09-23 00:31:52 +0400

    Update setup.py

--- setup/base_rest/setup.py
+++ setup/base_rest/setup.py
@@ -1,13 +1,13 @@
 import setuptools
 
 setuptools.setup(
-    # setup_requires=['setuptools-odoo'],
-    # odoo_addon={
-        # 'external_dependencies_override': {
-            # 'python': {
-                #'accept_language': 'parse-accept-language',
-                #'apispec': 'apispec==6.0.0'
-            # },
-        # },
-    # }
+    setup_requires=['setuptools-odoo==3.1.12'],
+    odoo_addon={
+        'external_dependencies_override': {
+            'python': {
+                'accept_language': 'parse-accept-language',
+                'apispec': 'apispec==6.3.0'
+            },
+        },
+    }
 )
